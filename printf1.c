e047a2bd63e337bfd7e3a3374fe627c4d952897a
Chrisli <chrislifortuin@gmail.com>
2023-08-25 16:34:03 -0700

    Education is when you read the fine print. Experience is what you get if you don't

---
+++ printf1.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Handling conversion specifiers
+ */
+int main()
+{
+	int num = 42;
+	int negnum = -10;
+
+	printf("%d\n", num);
+	printf("%i\n", negnum);
+
+	return (0);
+}
